fd9ab09a0f743c9e298df14746f2fb89f78514c3
Rajat Pandey <rajatpndey257@gmail.com>
2025-04-06 17:37:32 +0530

    User Authentication ,custom sign in and sign up pages, with clerk

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
     <main>
       <h1>Home</h1>
       <br />
-      <Button variant ="destructive">Subscribe</Button>
+      <Button variant = "destructive">Subscribe</Button>
     </main>
   );
 }
